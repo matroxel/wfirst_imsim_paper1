9c088ffd71b052b4b10d9752f631eb0220cb2c02
Michael Troxel <cond0083@ruby01.osc.edu>
2019-03-12 14:40:06 -0400

    push

--- code/fid_run.sh
+++ code/fid_run.sh
@@ -2,11 +2,11 @@
 #PBS -l nodes=1:ppn=20
 #PBS -A PCON0003
 #PBS -l walltime=5:00:00
-#PBS -t 1-403
+#PBS -t 1-189
 
 cd $PBS_O_WORKDIR
 export OMP_NUM_THREADS=1
 #psrecord --log memory_log.txt --interval 1.0 --include-children "python mcaltest.py"
 export PYTHONPATH=$PYTHONPATH:/users/PCON0003/cond0083/wfirst_imsim/
 
-mpiexec -n 20 python ../../wfirst_imsim/wfirst_imsim/simulate.py fid.yaml None $PBS_ARRAYID
+mpiexec -n 20 python ../../wfirst_imsim/wfirst_imsim/simulate.py fid.yaml None $PBS_ARRAYID verify_output
