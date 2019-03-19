c86dc4d172917ccf07bd805a4ef0b3f288f7db85
Michael Troxel <cond0083@ruby01.osc.edu>
2019-03-19 15:59:37 -0400

    push

---
+++ code/fid_run_short.sh
@@ -0,0 +1,13 @@
+#PBS -N fid
+#PBS -l nodes=1:ppn=20
+#PBS -A PCON0003
+#PBS -l walltime=4:00:00
+
+cd $PBS_O_WORKDIR
+export OMP_NUM_THREADS=1
+#psrecord --log memory_log.txt --interval 1.0 --include-children "python mcaltest.py"
+export PYTHONPATH=$PYTHONPATH:/users/PCON0003/cond0083/wfirst_imsim/
+
+mpiexec -n 20 python ../../wfirst_imsim/wfirst_imsim/simulate.py fid.yaml None 1 verify_output
+mpiexec -n 20 python ../../wfirst_imsim/wfirst_imsim/simulate.py fid.yaml None 2 verify_output
+mpiexec -n 20 python ../../wfirst_imsim/wfirst_imsim/simulate.py fid.yaml None 3 verify_output
