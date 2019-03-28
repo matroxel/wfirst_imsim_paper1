4087498567888b0d13ff91e71ca9c6db6f934898
Heyang Long <osu10670@ruby01.osc.edu>
2019-03-28 04:06:21 -0400

    push

--- code/fid_run.sh
+++ code/fid_run.sh
@@ -1,14 +1,8 @@
 #PBS -N fid
 #PBS -l nodes=1:ppn=20
-#PBS -m abe
 #PBS -A PCON0003
-<<<<<<< HEAD
-#PBS -l walltime=10:00:00
-#PBS -t 1
-=======
 #PBS -l walltime=5:00:00
 #PBS -t 1-189
->>>>>>> origin/master
 
 cd $PBS_O_WORKDIR
 export OMP_NUM_THREADS=1
