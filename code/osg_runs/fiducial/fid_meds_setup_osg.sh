b13a1ded4de20231ba481feb8317a16485e9428e
Michael Troxel <troxel@login.duke.ci-connect.net>
2019-06-14 14:13:49 +0000

    push

--- code/osg_runs/fiducial/fid_meds_setup_osg.sh
+++ code/osg_runs/fiducial/fid_meds_setup_osg.sh
@@ -23,23 +23,24 @@ request_memory = 4G
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT_OR_EVICT
 transfer_input_files    = /home/troxel/wfirst_stack/wfirst_stack.tar.gz, \
-                          /stash/user/troxel/wfirst_sim_input/, \
                           /home/troxel/wfirst_imsim_paper1/code/fid_osg.yaml, \
                           /home/troxel/wfirst_imsim_paper1/code/dither_list.txt,\
                           /home/troxel/wfirst_imsim_paper1/code/meds_pix_list.txt, \
                           /stash/user/troxel/wfirst_sim_fiducial/run.tar
 
-transfer_output_files   = truth/fiducial_H158_index_sorted.fits
+transfer_output_files   = truth/fiducial_H158_index_sorted.fits.gz
+transfer_output_remaps  = "fiducial_H158_index_sorted.fits.gz = /stash/user/troxel/wfirst_sim_fiducial/truth/fiducial_H158_index_sorted.fits.gz"
 
 Initialdir     = /stash/user/troxel/wfirst_sim_fiducial/
 log            = fid_meds_setup_log.log
 
 Arguments = fid_osg.yaml H158 meds setup
-Executable     = run_osg.sh
+Executable     = ../run_osg.sh
 Output         = fid_meds_setup.log
 Error          = fid_meds_setup.log
 
 # Science sensors only. Ignore 0,0 4,0 0,4 and 4,4. 
 
+priority = 1
 Queue
 
